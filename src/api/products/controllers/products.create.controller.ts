36f5fe95988d7ae0dcba2269c99c312d356db522
Franco Ferrari <francof2842@gmail.com>
2025-03-15 07:08:27 -0300

    arreglando servicio post

--- src/api/products/controllers/products.create.controller.ts
+++ src/api/products/controllers/products.create.controller.ts
@@ -1,7 +1,5 @@
-import type { Request, Response } from 'express'
 import { createProductService } from '../services/products.create.service'
 
-export const createProductController = (_req: Request, res: Response) => {
-	const newProduct = createProductService
-	res.json(newProduct)
+export const createProductController = (req: Request, res: Response) => {
+	createProductService(req, res);
 }
--- src/api/products/services/products.create.service.ts
+++ src/api/products/services/products.create.service.ts
@@ -1,13 +1,17 @@
 import { products } from '../../../data/products'
 import { productsRoutes } from '../products.routes'
 
-productsRoutes.post('/', (req, res) => {
+// El .post ya lo tenes en el archivo de routes, no hace falta que repitas eso
+/* productsRoutes.post('/', (req, res) => {
 	const newProduct = req.body
 	newProduct.id = products.length + 1
 	products.push(newProduct)
 	res.status(201).json(newProduct)
-})
+}) */
 
-export const createProductService = () => {
-	return products
+export const createProductService = (req: Request, res: Response) => {
+	const newProduct = req.body
+	newProduct.id = products.length + 1
+	products.push(newProduct)
+	res.status(201).json(newProduct)
 }
