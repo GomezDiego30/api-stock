c12d4a068af06932e971f6b6dac7eddd84aaa99d
Diego Gomez <dieguiito2010@gmail.com>
2025-03-25 10:52:19 -0300

    get en prisma

--- src/api/products/controllers/products.get.all.controller.ts
+++ src/api/products/controllers/products.get.all.controller.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express'
 import { getAllProductsService } from '../services/products.get.all.service'
 
-export const getAllProductsController = (req: Request, res: Response) => {
-	const products = getAllProductsService(req.body)
-	res.json(products)
+export const getAllProductsController = async (req: Request, res: Response) => {
+	try {
+		const products = await getAllProductsService(req.body)
+		res.json(products)
+	} catch (error) {
+		console.error('Error obteniendo los productos:', error)
+		res.status(500).json({ message: 'productos no obtenidos' })
+	}
 }
--- src/api/products/services/products.get.all.service.ts
+++ src/api/products/services/products.get.all.service.ts
@@ -1,5 +1,14 @@
-import { products } from '../../../data/products'
+import { PrismaClient } from '@prisma/client'
 
-export const getAllProductsService = (body: any) => {
-	return products
+const prisma = new PrismaClient()
+
+export const getAllProductsService = async (body: any) => {
+	try {
+		const products = await prisma.product.findMany({})
+
+		return products
+	} catch (error) {
+		console.error('Error getting products:', error)
+		throw new Error('Could not get products')
+	}
 }
